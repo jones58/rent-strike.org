7e6354971c7dfbf1fc31f03b08e5d60f11697fd0
jones58 <108466184+jones58@users.noreply.github.com>
2024-09-23 16:11:41 +0100

    add more archive files

---
+++ another copy web archive/web/20211101134325js_/https:/static1.squarespace.com/static/ta/4f6798afe4b097349e410d49/1598/scripts/combo/index.html?site.js
@@ -0,0 +1,35 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+(function(){if(!window.document.querySelector)return!1;document.addEventListener("DOMContentLoaded",function(){var a=document.getElementById("mobileSelect");if(!a)return!1;a.addEventListener("change",function(){window.location=a.options[a.selectedIndex].value})});window.addEventListener("resize",function(){for(var a=document.querySelectorAll("img[data-src]"),b=0;b<a.length;b++)ImageLoader.load(a[b])})})();
+
+}
+/*
+     FILE ARCHIVED ON 12:57:03 Nov 01, 2021 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 11:20:16 Sep 18, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.637
+  exclusion.robots: 0.02
+  exclusion.robots.policy: 0.01
+  esindex: 0.01
+  cdx.remote: 5.958
+  LoadShardBlock: 296.887 (6)
+  PetaboxLoader3.datanode: 400.575 (8)
+  PetaboxLoader3.resolve: 171.431 (3)
+  load_resource: 307.02 (2)
+*/
