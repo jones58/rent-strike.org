a571e3c0724f25e80c77833f7f49a138d43b8f27
jones58 <108466184+jones58@users.noreply.github.com>
2024-10-17 09:21:27 +0100

    add custom css styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,27 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
-import starlightThemeRapide from "starlight-theme-rapide";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     starlight({
-      plugins: [starlightThemeRapide()],
       title: "RENT STRIKE",
+      customCss: ["./src/styles.css"],
       tableOfContents: false,
       sidebar: [
         {
           label: "Rent Strike",
           autogenerate: { directory: "all" },
+        },
+      ],
+      head: [
+        {
+          tag: "script",
+          attrs: {
+            src: "https://umami.jackkershaw.pp.ua/script.js",
+            "data-website-id": "50b65072-e17d-480e-9acd-29269b26690e",
+            defer: true,
+          },
         },
       ],
     }),
